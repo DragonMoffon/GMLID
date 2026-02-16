ca87954b48132e30b1a1da905367cc05445cb2f5
DragonMoffon <happydog.aja@gmail.com>
2026-02-16 14:15:52 +1300

    Context using DataSubject
    paused until analytical comparison is completed

--- GMLID/context.py
+++ GMLID/context.py
@@ -7,27 +7,29 @@ from .system import Lens
 
 WINDOW_TITLE = ""
 
+
 class GMLIDContext(DataSubject):
     schema: DataSchema = {
-        "window": { # The actual window config
+        "window": {  # The actual window config
             "width": ("window_width", 720),
             "height": ("window_height", 405),
             "fullscreen": ("window_fullscreen", False),
         },
-        "system": { # The lenses active for GMLID
-            "distance": ("lens_distance", 4000),
+        "system": {  # The lenses active for GMLID
+            "lens_distance": ("lens_distance", 4000),
+            "source_distance": ("source_dstance", 8000),
+            "image_size": ("lens_image_size", (4000, 4000)),
         },
-        "source": { # Info about the source star
-            "distance": ("source_distance", 8000),
+        "histogram": {  # Control variables for the histogram
+            "ray_count": ("histogram_ray_count", 10000),
+            "image_size": ("histogram_image_size", (2048, 2048)),
         },
-        "histogram": { # Control variables for the histogram
+        "event": {  # Control variables for the lens event system
         },
-        "event": { # Control variables for the lens event system
-        }
     }
 
     def __init__(self, values: dict[str, Any] | None = None) -> None:
-        DataSchema.__init__(self, values, print)
+        DataSubject.__init__(self, values, print)
 
         # Window Config
         self.window_width: int
@@ -37,8 +39,17 @@ class GMLIDContext(DataSubject):
         # Lens System Config
         self.lens_distance: float
         self.source_distance: float
+        self.lens_image_size: tuple[int, int]
         self.lenses: tuple[Lens, ...]
 
         # Histogram Config
+        self.histogram_ray_count: int
+        self.histogram_image_size: tuple[int, int]
 
         # Lens Event Config
+
+    def pre_process(self):
+        pass
+
+    def post_process(self):
+        pass
