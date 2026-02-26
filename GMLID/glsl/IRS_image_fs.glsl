1e3ec06101e59febcd4ba9fb2730a4037aacd036
DragonMoffon <happydog.aja@gmail.com>
2026-02-27 10:28:09 +1300

    remap deflection map to show -2.0 to 2.0 -> 0.0 to 1.0 rather than -1.0 to 1.0.

--- GMLID/glsl/IRS_image_fs.glsl
+++ GMLID/glsl/IRS_image_fs.glsl
@@ -10,7 +10,9 @@ out vec4 fs_colour;
 void main(){
   vec2 pos = texture(texture0, vs_uv).xy;
 
-  fs_colour = texture(texture0, vs_uv)*0.5 + 0.5 + vec4(vec3(1.0 - step(source.z, length(pos - source.xy))), 1.0);
-
+  fs_colour = texture(texture0, vs_uv)*0.25 + 0.5 + vec4(vec3(1.0 - step(source.z, length(pos - source.xy))), 1.0);
+  /*if (fs_colour.r < 0.0 || 1.0 < fs_colour.r || fs_colour.g < 0.0 || 1.0 < fs_colour.g){
+    fs_colour = vec4(0.0, 0.0, 0.0, 1.0);
+  }*/
   //fs_colour = texture(texture0, vs_uv);
 }
