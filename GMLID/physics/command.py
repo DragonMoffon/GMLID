50d60c6f910e6962a02dd33ed86e811569c7cb3f
DragonMoffon <happydog.aja@gmail.com>
2026-03-16 01:18:12 +1300

    started on parse args function for GMLID command line mode (future update)

---
+++ GMLID/physics/command.py
@@ -0,0 +1,14 @@
+from typing import Sequence
+import argparse
+
+
+def parse_args(args: Sequence[str] | None = None):
+    parser = argparse.ArgumentParser(
+        "GMLID",
+        "Gravitational Lensing Interactive Demo for thin-lens simulations",
+        allow_abbrev=True,
+    )
+
+    parser.add_argument("--launch", action="store_true", help="Launch the real-time demo")
+
+    arguments = parser.parse_args(args)
